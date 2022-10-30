2342416ac3cf1ce8a0fb6f7c5683fa268c0ffdf4
shafik720 <shafikrasel5@gmail.com>
2022-10-30 14:22:08 +0600

    dfgdg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router';
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Welcome to React Router</h1>
+      <Routes>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="/about" element={<About></About>} />
+      </Routes>
     </div>
   );
 }
