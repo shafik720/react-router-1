277da117607dfc9c0c66ad011d4c6cdf956904a7
shafik720 <shafikrasel5@gmail.com>
2022-10-30 15:58:19 +0600

    dfgdg

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import About from './Components/About/About';
 import Contact from './Components/Contact/Contact';
 import Error from './Components/Error/Error';
 import Friends from './Components/Friends/Friends';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to React Router</h1>
+      <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>} />
         <Route path="/about" element={<About></About>} />
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <Link to="/" >Home</Link>
+            <Link to="/about" >About</Link>
+            <Link to="/contact" >Contact</Link>            
+            <Link to="/friends" >Friend</Link>            
+        </div>
+    );
+};
+
+export default Header;
