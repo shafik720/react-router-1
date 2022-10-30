36327f401daa4fa43878f54d414698d6191fc557
shafik720 <shafikrasel5@gmail.com>
2022-10-30 15:03:19 +0600

    dfgdg

--- src/Components/Friend/Friend.js
+++ src/Components/Friend/Friend.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
 
 const Friend = (props) => {
     const{name, id, username} = props.index
+    let navigate =  useNavigate();
+    function showUser(){
+        let path = `/friend/${id}`;
+        navigate(path);
+    }
     return (
         <div>
             <h2>Name : {name} </h2>
+            <button onClick={showUser}>Show User</button>
         </div>
     );
 };
