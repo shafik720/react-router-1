31a096e77810b2096548caf67b2811a6f3b39aa3
shafik720 <shafikrasel5@gmail.com>
2022-10-30 14:49:43 +0600

    dfgdg

--- src/Components/Friends/Friends.js
+++ src/Components/Friends/Friends.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Friend from '../Friend/Friend';
 
 const Friends = () => {
+    const[friends, setFriends] = useState([]);
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res=>res.json())
+        .then(data=>setFriends(data))
+    },[])
     return (
         <div>
-            <h2>Hello Friends</h2>
+            <h2>Hello Friends : {friends.length} </h2>
+            {
+                friends.map(index=><Friend
+                    key = {index.id}
+                    index={index}
+                ></Friend>)
+            }
         </div>
     );
 };
