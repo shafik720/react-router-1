258f3bdd867e562517ee216741c2eb99bc76003a
shafik720 <shafikrasel5@gmail.com>
2022-10-30 14:24:02 +0600

    dfgdg

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Routes } from 'react-router';
 import Home from './Components/Home/Home';
 import About from './Components/About/About';
+import Contact from './Components/Contact/Contact';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>} />
         <Route path="/about" element={<About></About>} />
+        <Route path="/contact" element={<Contact></Contact>} ></Route>
       </Routes>
     </div>
   );
