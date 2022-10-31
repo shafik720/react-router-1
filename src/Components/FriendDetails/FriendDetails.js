32c01aaaa1855ab1eeaf34927a15e2668c3fe385
shafik720 <shafikrasel5@gmail.com>
2022-10-31 10:47:04 +0600

    dxfgdsg

--- src/Components/FriendDetails/FriendDetails.js
+++ src/Components/FriendDetails/FriendDetails.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useParams } from 'react-router';
 
 const FriendDetails = () => {
+    let param = useParams();
     return (
         <div>
-            <h3>Friend Details</h3>
+            <h3>Friend Details : {param.details}</h3>
         </div>
     );
 };
