8eeeb5a92981581e36674caba505c31f6c25ad3b
shafik720 <shafikrasel5@gmail.com>
2022-10-31 11:11:22 +0600

    dxfgdsg

--- src/Components/PostDetails/PostDetails.js
+++ src/Components/PostDetails/PostDetails.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useParams } from 'react-router';
 
 const PostDetails = () => {
+    let param = useParams();
     return (
         <div>
-            <h2>Post Details</h2>
+            <h2>Post Details for : {param.postDetails} </h2>
         </div>
     );
 };
