a7ca0e4cf0b6b55045c93e5a5170cd0201d8e7db
shafik720 <shafikrasel5@gmail.com>
2022-10-31 10:57:13 +0600

    dxfgdsg

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Post = () => {
+    let [post, setPost] = useState([]);
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res=>res.json())
+        .then(data=>console.log(data))
+    },[])
     return (
         <div>
             <h2>Hello From Posts</h2>
